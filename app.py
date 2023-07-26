4f64a5b05f447dd726ea20c71af762171dd6ea46
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2023-07-26 07:40:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,11 @@
-from flask import Flask, request, jsonify
+# app.py
+from flask import Flask
 
 app = Flask(__name__)
-todos = []
 
-
-@app.route('/todos', methods=['GET'])
-def get_todos():
-    return jsonify(todos)
-
-
-@app.route('/todos', methods=['POST'])
-def add_todo():
-    data = request.get_json()
-    if 'task' in data:
-        task = data['task']
-        todos.append(task)
-        return jsonify({"message": "Todo added successfully."})
-    else:
-        return jsonify({"error": "Invalid request data."}), 400
-
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
